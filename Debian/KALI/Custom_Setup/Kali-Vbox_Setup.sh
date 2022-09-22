95ac550d1a9205ab0a2689b956a4d684fcf9d3f4
MIGIC <holocron.ltd@gmail.com>
2022-09-22 05:39:36 +0100

    fixed get pub IP alias

--- Debian/KALI/Custom_Setup/Kali-Vbox_Setup.sh
+++ Debian/KALI/Custom_Setup/Kali-Vbox_Setup.sh
@@ -46,6 +46,8 @@ logo () {
 
 
 Ease_Of_Life () {
+    logo
+    sleep 2.5
     echo kali | sudo -S apt-get update && sudo apt-get -y upgrade
     sudo apt-get install -y toilet
     sudo apt-get install -y lolcat
@@ -355,45 +357,46 @@ Aliases () {
     echo kali | sudo cp .bashrc .bashrc.backup
     echo kali | sudo cp .zshrc .zshrc.backup
 
-    echo "\n\n#ODYSSEUS'S CUSTOM COMMANDS LIST\n" && sudo tee -a .zshrc
-    echo "#Improved Ease Of Use\n" && sudo tee -a .zshrc
+    echo "\n\n#ODYSSEUS'S CUSTOM COMMANDS LIST\n" | sudo tee -a .zshrc
+    echo "#Improved Ease Of Use\n" | sudo tee -a .zshrc
 
     #SYSTEM
-    echo "alias lt='ls --human-readable --size -1 -S --classify' #Improved File Visability & Layout" && sudo tee -a .zshrc
-    echo "alias upgrade='sudo apt-get update && sudo apt-get upgrade' #Update And Upgrade In One Command" && sudo tee -a .zshrc
-    echo "alias full-upgrade='sudo apt full-upgrade' #Full System Upgrade" && sudo tee -a .zshrc
-    echo "alias ports='netstat -tulanp' #Lists all open ports" && sudo tee -a .zshrc
-    echo "alias c='clear' #Shorterns The Clear Command" && sudo tee -a .zshrc
-    echo "alias cleardown='cd ~/Downloads && rm -r *' #Clears downloads folder" && sudo tee -a .zshrc
-    echo "alias cls='clear && ls' #Clears & Lists Files" && sudo tee -a .zshrc
-    #echo "alias shortName='Command_Here'" && sudo tee -a .zshrc
+    echo "alias lt='ls --human-readable --size -1 -S --classify' #Improved File Visability & Layout" | sudo tee -a .zshrc
+    echo "alias upgrade='sudo apt-get update && sudo apt-get upgrade' #Update And Upgrade In One Command" | sudo tee -a .zshrc
+    echo "alias full-upgrade='sudo apt full-upgrade' #Full System Upgrade" | sudo tee -a .zshrc
+    echo "alias ports='netstat -tulanp' #Lists all open ports" | sudo tee -a .zshrc
+    echo "alias c='clear' #Shorterns The Clear Command" | sudo tee -a .zshrc
+    echo "alias cleardown='cd ~/Downloads && rm -r *' #Clears downloads folder" | sudo tee -a .zshrc
+    echo "alias cls='clear && ls' #Clears & Lists Files" | sudo tee -a .zshrc
+    #echo "alias shortName='Command_Here'" | sudo tee -a .zshrc
 
     #PYTHON
-    echo "alias ve='python3 -m venv ./venv' #Creates A Python Virtual Environment In Current Dir" && sudo tee -a .zshrc
-    echo "alias va='source ./venv/bin/activate' #Activates The Python venv" && sudo tee -a .zshrc
-    echo "alias reqs='python3 -m pip install requirements.txt' #Installs Requirements.txt From Current Dir" && sudo tee -a .zshrc
-    #echo "alias shortName='Command_Here'" && sudo tee -a .zshrc
+    echo "alias ve='python3 -m venv ./venv' #Creates A Python Virtual Environment In Current Dir" | sudo tee -a .zshrc
+    echo "alias va='source ./venv/bin/activate' #Activates The Python venv" | sudo tee -a .zshrc
+    echo "alias reqs='python3 -m pip install requirements.txt' #Installs Requirements.txt From Current Dir" | sudo tee -a .zshrc
+    #echo "alias shortName='Command_Here'" | sudo tee -a .zshrc
 
     #OTHER (UNFINISHED)
-    echo "alias csec='cd ~/CyberSec'" && sudo tee -a .zshrc
-    echo "alias reps='cd ~/Reports'" && sudo tee -a .zshrc
-    echo "alias ghub='cd ~/Github'" && sudo tee -a .zshrc
-    echo "alias projects='cd ~/Projects'" && sudo tee -a .zshrc
-    echo "alias ody='toilet ODYSSEUS.. -f smblock -w 500 | lolcat'" && sudo tee -a .zshrc
-    echo "alias legend='toilet LIVING LEGEND.. -f smblock -w 500 | lolcat'" && sudo tee -a .zshrc
-    #echo "alias shortName='Command_Here'" && sudo tee -a .zshrc
+    echo "alias csec='cd ~/CyberSec'" | sudo tee -a .zshrc
+    echo "alias reps='cd ~/Reports'" | sudo tee -a .zshrc
+    echo "alias ghub='cd ~/Github'" | sudo tee -a .zshrc
+    echo "alias projects='cd ~/Projects'" | sudo tee -a .zshrc
+    echo "alias ody='toilet ODYSSEUS.. -f smblock -w 500 | lolcat'" | sudo tee -a .zshrc
+    echo "alias legend='toilet LIVING LEGEND.. -f smblock -w 500 | lolcat'" | sudo tee -a .zshrc
+    #echo "alias shortName='Command_Here'" | sudo tee -a .zshrc
 
     #ADVANCED ALIASES (FUNCTIONS)
-    echo "\n\n #ADVANCED ALIASES (FUNCTIONS)\n" && sudo tee -a .zshrc
-    echo "#SHOWS PUBLIC IP WHICH ISNT SUPPORTED BY IPCONFIG" && sudo tee -a .zshrc
-    echo "pubip(){" && sudo tee -a .zshrc
-    echo "   pubip='$(dig +short myip.opendns.com @resolver1.opendns.com)'" && sudo tee -a .zshrc
-    echo "   echo 'My WAN/Public IP address: \${pubip}'\n}" && sudo tee -a .zshrc
-    echo "\n\n" && sudo tee -a .zshrc
-    echo "" && sudo tee -a .zshrc
+    echo '\n\n #ADVANCED ALIASES (FUNCTIONS)\n' | sudo tee -a .zshrc
+
+    echo '#SHOWS PUBLIC IP WHICH ISNT SUPPORTED BY IPCONFIG' | sudo tee -a .zshrc
+    echo 'pubip(){' | sudo tee -a .zshrc
+    echo '   pubip="$(dig +short myip.opendns.com @resolver1.opendns.com)"' | sudo tee -a .zshrc
+    echo '   echo Public IP Address: "\e[1;4;32m${pubip}\e[0m"' | sudo tee -a .zshrc
+    echo    "\n}" | sudo tee -a .zshrc
+#    echo \n\n | sudo tee -a .zshrc
 
 
-    echo "\n###### END OF ODY'S CUSTOM COMMANDS #####\n\n" && sudo tee -a .zshrc
+    echo "\n###### END OF ODY'S CUSTOM COMMANDS #####\n\n" | sudo tee -a .zshrc
     
     cd ~
     #Updates The Local Profile Alias's
@@ -610,3 +613,10 @@ make custom menu folder & Shortcuts
 
 # Refrences
 # https://newbedev.com/shopt-command-not-found-in-bashrc-after-shell-updation
+# https://www.shellhacks.com/bash-colors/
+
+#NOTE TO SELF
+# Double quotes are only for reffrencing Variables eg "${VAR}" is valid in a function
+# Where as ${VAR} On Its Own without the "" is not.
+# Use Single Quotes For Strings eg 'Hello World.'
+# And all together: 'Hello "${VAR}"'
